648c967dc51c9cc4c910d680e66d3a5fc0846484
Distant22 <sindistant@gmail.com>
2023-04-20 15:34:36 +0800

    topbar被我改了一個字

--- src/components/topbar.jsx
+++ src/components/topbar.jsx
@@ -3,7 +3,7 @@ export default function TopBar() {
       <>
         <div class="h-32 bg-topbar flex items-center ">
             <div class="ml-10 text-3xl font-bold">
-                你的 AI 旅遊小幫手
+                你的 AI 旅遊大幫手
             </div>
         </div>
       </>
